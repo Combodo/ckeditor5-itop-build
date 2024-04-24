000d82ff3dea3df6b87aaa8ac60c06bfbce9986d
Stephen Abello <stephen.abello@combodo.com>
2024-04-24 10:28:06 +0200

    Add CKEditor 5 sources and our plugins

---
+++ src/plugins/append-itop-classes/append-itop-classes.plugin.ts
@@ -0,0 +1,31 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+
+/**
+ * AppendITopClasses Plugin.
+ *
+ * Appends ibo-is-html-content class
+ */
+export default class AppendITopClasses extends Plugin {
+    // @ts-ignore
+    constructor(editor) {
+        super(editor);
+    }
+    static get pluginName() {
+        return 'AppendITopClasses';
+    }
+
+    init() {
+
+        // retrieve editor instance
+        const oEditor = this.editor;
+
+        // appends ibo-is-html-content class
+        oEditor.editing.view.change( oWriter => {
+            const oRootElement = oEditor.editing.view.document.getRoot();
+            if(oRootElement !== null){
+                oWriter.addClass( 'ibo-is-html-content', oRootElement);
+            }
+        });
+    }
+}
+
