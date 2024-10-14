c26c1ce1d07284d49c3749f14eb1ebbafebecd4d
Jeffrey Bostoen <support@jeffreybostoen.be>
2024-10-14 19:02:31 +0200

    Fix: Content should be inserted at the cursor position; not at the end.

--- src/plugins/insert-html/insert-html.command.ts
+++ src/plugins/insert-html/insert-html.command.ts
@@ -7,7 +7,15 @@ import { Command } from 'ckeditor5/src/core';
 export default class InsertHtmlCommand extends Command {
 
     override execute( sContent:string ) {
-        this.editor.setData(this.editor.getData() + sContent);
+	
+		const viewFragment = this.editor.data.processor.toView(sContent)
+		const modelFragment = this.editor.data.toModel(viewFragment)
+
+		this.editor.model.change(writer => {
+			const insertPosition = this.editor.model.document.selection.getFirstPosition()
+			writer.insert(modelFragment, insertPosition)
+		})
+		
     }
 }
 
